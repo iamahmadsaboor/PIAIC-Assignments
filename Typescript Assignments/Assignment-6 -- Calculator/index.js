85f730c411581e04103c925c714bd2a0eaee4cf9
Ahmad Saboor <iamahmadsaboor@gmail.com>
2023-09-18 23:09:34 +0500

    Object Destructuring DOne

--- Typescript Assignments/Assignment-6 -- Calculator/index.js
+++ Typescript Assignments/Assignment-6 -- Calculator/index.js
@@ -1,78 +1,60 @@
 import inquirer from "inquirer";
 const calcInput = async () => {
-  let value1 = await inquirer.prompt([
-    {
-      type: "number",
-      name: "number_1",
-      message: "Please Enter Number 1",
-    },
-  ]);
-  let value2 = await inquirer.prompt([
-    {
-      type: "number",
-      name: "number_2",
-      message: "Please Enter Number 2",
-    },
-  ]);
-  let operator_All = await inquirer.prompt([
-    {
-      type: "list",
-      name: "operator",
-      message: "Please Select Operator",
-      choices: ["+", "-", "*", "/", "^", "%"],
-    },
-  ]);
-  if (operator_All.operator == "+") {
-    console.log(
-      `${value1.number_1} ${operator_All.operator} ${value2.number_2} = ${
-        value1.number_1 + value2.number_2
-      }`
-    );
-  } else if (operator_All.operator == "-") {
-    console.log(
-      `${value1.number_1} ${operator_All.operator} ${value2.number_2} = ${
-        value1.number_1 - value2.number_2
-      }`
-    );
-  } else if (operator_All.operator == "*") {
-    console.log(
-      `${value1.number_1} ${operator_All.operator} ${value2.number_2} = ${
-        value1.number_1 * value2.number_2
-      }`
-    );
-  } else if (operator_All.operator == "/") {
-    console.log(
-      `${value1.number_1} ${operator_All.operator} ${value2.number_2} = ${
-        value1.number_1 / value2.number_2
-      }`
-    );
-  } else if (operator_All.operator == "^") {
-    console.log(
-      `${value1.number_1} ${operator_All.operator} ${value2.number_2} = ${
-        value1.number_1 ** value2.number_2
-      }`
-    );
-  } else if (operator_All.operator == "%") {
-    console.log(
-      `${value1.number_1} ${operator_All.operator}  ${value2.number_2} = ${
-        value1.number_1 % value2.number_2
-      }`
-    );
-  } else {
-    console.log("Invalid!!");
-  }
-  let ask = await inquirer.prompt([
-    {
-      type: "confirm",
-      name: "again",
-      message: "Do you want to continue?", // Message to show the user
-      //   default: true, // Default value of the prompt
-    },
-  ]);
-  if (ask.again == true) {
-    calcInput();
-  } else {
-    console.log(`Thanks for Using Our Calculator -- iamahmadsaboor`);
-  }
+    const { number_1 } = await inquirer.prompt([
+        {
+            type: "number",
+            name: "number_1",
+            message: "Please Enter Number 1",
+        },
+    ]);
+    const { number_2 } = await inquirer.prompt([
+        {
+            type: "number",
+            name: "number_2",
+            message: "Please Enter Number 2",
+        },
+    ]);
+    const { operator } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "operator",
+            message: "Please Select Operator",
+            choices: ["+", "-", "*", "/", "^", "%"],
+        },
+    ]);
+    if (operator == "+") {
+        console.log(`${number_1} ${operator} ${number_2} = ${number_1 + number_2}`);
+    }
+    else if (operator == "-") {
+        console.log(`${number_1} ${operator} ${number_2} = ${number_1 - number_2}`);
+    }
+    else if (operator == "*") {
+        console.log(`${number_1} ${operator} ${number_2} = ${number_1 * number_2}`);
+    }
+    else if (operator == "/") {
+        console.log(`${number_1} ${operator} ${number_2} = ${number_1 / number_2}`);
+    }
+    else if (operator == "^") {
+        console.log(`${number_1} ${operator} ${number_2} = ${number_1 ** number_2}`);
+    }
+    else if (operator == "%") {
+        console.log(`${number_1} ${operator}  ${number_2} = ${number_1 % number_2}`);
+    }
+    else {
+        console.log("Invalid!!");
+    }
+    let ask = await inquirer.prompt([
+        {
+            type: "confirm",
+            name: "again",
+            message: "Do you want to continue?",
+        },
+    ]);
+    if (ask.again == true) {
+        calcInput();
+    }
+    else {
+        console.log(`Thanks for Using Our Calculator -- iamahmadsaboor`);
+    }
 };
 calcInput();
--- Typescript Assignments/Assignment-6 -- Calculator/index.ts
+++ Typescript Assignments/Assignment-6 -- Calculator/index.ts
@@ -1,21 +1,27 @@
 import inquirer from "inquirer";
 
 const calcInput = async () => {
-  let value1 = await inquirer.prompt([
+  // Below Done Object Destructuring
+
+  const { number_1 } = await inquirer.prompt([
     {
       type: "number",
       name: "number_1",
       message: "Please Enter Number 1",
     },
   ]);
-  let value2 = await inquirer.prompt([
+  // Below Done Object Destructuring
+
+  const { number_2 } = await inquirer.prompt([
     {
       type: "number",
       name: "number_2",
       message: "Please Enter Number 2",
     },
   ]);
-  let operator_All = await inquirer.prompt([
+  // Below Done Object Destructuring
+
+  const { operator } = await inquirer.prompt([
     {
       type: "list",
       name: "operator",
@@ -24,41 +30,21 @@ const calcInput = async () => {
     },
   ]);
 
-  if (operator_All.operator == "+") {
+  if (operator == "+") {
+    console.log(`${number_1} ${operator} ${number_2} = ${number_1 + number_2}`);
+  } else if (operator == "-") {
+    console.log(`${number_1} ${operator} ${number_2} = ${number_1 - number_2}`);
+  } else if (operator == "*") {
+    console.log(`${number_1} ${operator} ${number_2} = ${number_1 * number_2}`);
+  } else if (operator == "/") {
+    console.log(`${number_1} ${operator} ${number_2} = ${number_1 / number_2}`);
+  } else if (operator == "^") {
     console.log(
-      `${value1.number_1} ${operator_All.operator} ${value2.number_2} = ${
-        value1.number_1 + value2.number_2
-      }`
+      `${number_1} ${operator} ${number_2} = ${number_1 ** number_2}`
     );
-  } else if (operator_All.operator == "-") {
+  } else if (operator == "%") {
     console.log(
-      `${value1.number_1} ${operator_All.operator} ${value2.number_2} = ${
-        value1.number_1 - value2.number_2
-      }`
-    );
-  } else if (operator_All.operator == "*") {
-    console.log(
-      `${value1.number_1} ${operator_All.operator} ${value2.number_2} = ${
-        value1.number_1 * value2.number_2
-      }`
-    );
-  } else if (operator_All.operator == "/") {
-    console.log(
-      `${value1.number_1} ${operator_All.operator} ${value2.number_2} = ${
-        value1.number_1 / value2.number_2
-      }`
-    );
-  } else if (operator_All.operator == "^") {
-    console.log(
-      `${value1.number_1} ${operator_All.operator} ${value2.number_2} = ${
-        value1.number_1 ** value2.number_2
-      }`
-    );
-  } else if (operator_All.operator == "%") {
-    console.log(
-      `${value1.number_1} ${operator_All.operator}  ${value2.number_2} = ${
-        value1.number_1 % value2.number_2
-      }`
+      `${number_1} ${operator}  ${number_2} = ${number_1 % number_2}`
     );
   } else {
     console.log("Invalid!!");
@@ -66,10 +52,9 @@ const calcInput = async () => {
 
   let ask = await inquirer.prompt([
     {
-      type: "confirm", // Type of prompt to show the user, can be input or confirm etc..
-      name: "again", // Name of the variable to store the user input
-      message: "Do you want to continue?", // Message to show the user
-      //   default: true, // Default value of the prompt
+      type: "confirm",
+      name: "again",
+      message: "Do you want to continue?",
     },
   ]);
 
