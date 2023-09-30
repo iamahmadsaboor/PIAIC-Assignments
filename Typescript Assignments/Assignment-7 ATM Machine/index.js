06da5d5dd8c80f61b2f8bc5ffaf761839cea81a5
Ahmad Saboor <iamahmadsaboor@gmail.com>
2023-09-30 13:36:52 +0500

    modified:   Typescript Assignments/Assignment-7 ATM Machine/index.js 	modified:   Typescript Assignments/Assignment-7 ATM Machine/index.ts

--- Typescript Assignments/Assignment-7 ATM Machine/index.js
+++ Typescript Assignments/Assignment-7 ATM Machine/index.js
@@ -1,17 +1,11 @@
 import inquirer from "inquirer";
-console.log(`Welcome to Heavy Bank To Continue Please Insert `);
+console.log(`Welcome to Heavy Bank To Continue Please Enter Your MPIN  `);
 const userData = [
     {
         userId: 1,
         userName: "Ahmad",
         pin: "1245",
         balance: 100000000,
-    },
-    {
-        userId: 2,
-        userName: "Ali",
-        pin: "4212",
-        balance: 200000,
     },
 ];
 const { userId, userName, pin, balance } = userData[0];
@@ -20,25 +14,14 @@ const answers = inquirer.prompt([
         type: "string",
         name: "pin",
         message: "Enter Your 4 digit pin",
-        maxLength: 4,
-    },
-    {
-        type: "confirm",
-        name: "confirm",
-        message: "Do you want to check your balance?",
     },
 ]);
 answers.then((answer) => {
     if (pin == answer.pin) {
-        if (answer.pin > 4) {
-            console.log("invalid Pin Enter 4 Digit Pin Code");
-        }
-        else {
-            console.log("Welcome " + userName);
-            if (answer.confirm == true && pin == answer.pin) {
-                console.log(`Your Balance is ${balance}`);
-            }
-        }
+        // if (answer.pin >= 5) {
+        //   console.log("invalid Pin Enter 4 Digit Pin Code");
+        // }
+        console.log("Welcome " + userName);
     }
     else {
         console.log("Wrong  Pincode");
--- Typescript Assignments/Assignment-7 ATM Machine/index.ts
+++ Typescript Assignments/Assignment-7 ATM Machine/index.ts
@@ -9,12 +9,6 @@ const userData = [
     pin: "1245",
     balance: 100000000,
   },
-  {
-    userId: 2,
-    userName: "Ali",
-    pin: "4212",
-    balance: 200000,
-  },
 ];
 const { userId, userName, pin, balance } = userData[0];
 const answers = inquirer.prompt([
@@ -22,24 +16,11 @@ const answers = inquirer.prompt([
     type: "string",
     name: "pin",
     message: "Enter Your 4 digit pin",
-    maxLength: 4,
-  },
-  {
-    type: "confirm",
-    name: "confirm",
-    message: "Do you want to check your balance?",
   },
 ]);
 answers.then((answer) => {
-  if (pin == answer.pin) {
-    if (answer.pin > 4) {
-      console.log("invalid Pin Enter 4 Digit Pin Code");
-    } else {
-      console.log("Welcome " + userName);
-      if (answer.confirm == true && pin == answer.pin) {
-        console.log(`Your Balance is ${balance}`);
-      }
-    }
+  if (pin == answer.pin && answer.pin == 4) {
+    console.log("Welcome " + userName);
   } else {
     console.log("Wrong  Pincode");
   }
