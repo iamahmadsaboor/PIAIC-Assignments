17e10811b326f9dc8870d208d44b5199f59357e3
Ahmad Saboor <iamahmadsaboor@gmail.com>
2023-10-01 23:35:40 +0500

    Transaction Added

--- Typescript Assignments/Assignment-7 ATM Machine/index.ts
+++ Typescript Assignments/Assignment-7 ATM Machine/index.ts
@@ -11,55 +11,131 @@ const user = {
 
 const { accountNumber, userName, pin, balance } = user;
 
-// User Input
-const answers = inquirer.prompt([
-  {
-    type: "number",
-    name: "accNumb",
-    message: "Enter Your Account Number",
-  },
-  {
-    type: "number",
-    name: "accPin",
-    message: "Enter Your 4 digit pin",
-  },
-]);
 // Balance Checking Functionality
-const balanceChecker = () => {
-  const balcInquirer = inquirer.prompt([
+const balanceChecker = async () => {
+  const balcInquirer = await inquirer.prompt([
     {
       type: "confirm",
       name: "opt",
       message: "Do You Want to check the Balance?",
     },
   ]);
-  balcInquirer.then((balc) => {
-    const { opt } = balc;
-    if (opt) {
-      console.log("Your Balance is " + balance);
+  const { opt } = balcInquirer;
+  if (opt) {
+    console.log("Your Balance is " + balance);
+  } else {
+    console.log("Thank You For Using Heavy Bank");
+  }
+};
+//  Transaction
+const performTransaction = async () => {
+  const transactionRecord: number[] = [];
+  const transaction = await inquirer.prompt([
+    {
+      type: "list",
+      name: "trans",
+      message: "Do You want to perform a transaction",
+      choices: ["Withdraw", "Deposit"],
+    },
+  ]);
+  const { trans } = transaction;
+
+  if (trans == "Withdraw") {
+    const withdraw = await inquirer.prompt([
+      {
+        type: "number",
+        name: "amount",
+        message: "Enter the amount you want to withdraw",
+      },
+    ]);
+    const { amount } = withdraw;
+    if (amount > balance) {
+      console.log("Insufficient Balance");
+    } else {
+      console.log("Your Remaining Balance is " + (balance - amount));
+      transactionRecord.push(balance - amount);
+    }
+  } else {
+    if (trans == "Deposit") {
+      const deposit = await inquirer.prompt([
+        {
+          type: "number",
+          name: "depAmount",
+          message: "Enter the amount you want to deposit",
+        },
+      ]);
+      const { depAmount } = deposit;
+      if (depAmount > 0) {
+        console.log("Your New Balance is " + (balance + depAmount));
+        transactionRecord.push(balance + depAmount);
+      } else {
+        console.log("Wrong Input");
+      }
+    }
+  }
+  console.log(transactionRecord.length);
+  if (transactionRecord.length >= 5) {
+    const chkRecord = await inquirer.prompt([
+      {
+        type: "confirm",
+        name: "chk",
+        message: "Do you want to check your last 5 transactions",
+      },
+    ]);
+    const { chk } = chkRecord;
+    if (chk) {
+      console.log(transactionRecord);
     } else {
       console.log("Thank You For Using Heavy Bank");
     }
-  });
+  }
 };
-//  Transaction
-
-const transaction = await inquirer.prompt([
-  {
-    type: "list",
-    name: "trans",
-    message: "What do you want to transact?",
-    choices: ["Withdraw", "Deposit"],
-  },
-]);
-
 // Main Functionality
-answers.then((answer) => {
-  const { accNumb, accPin } = answer;
+
+// User Input
+const mainFunc = async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: "number",
+      name: "accNumb",
+      message: "Enter Your Account Number",
+    },
+    {
+      type: "number",
+      name: "accPin",
+      message: "Enter Your 4 digit pin",
+    },
+  ]);
+  const { accNumb, accPin } = answers;
+
   if (pin == accPin && accountNumber == accNumb) {
     console.log("Welcome " + userName);
-    balanceChecker();
+    setTimeout(async () => {
+      await performTransaction();
+      await balanceChecker();
+      moreTransaction();
+    }, 2000);
   } else {
-    console.log("Wrong");
+    console.log("Wrong Input");
   }
-});
+};
+const moreTransaction = async () => {
+  const moreTrans = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "more",
+      message: "Do you want to perform more transactions",
+    },
+  ]);
+  const { more } = moreTrans;
+  if (!more) {
+  } else {
+    while (more == true) {
+      await performTransaction();
+      await moreTransaction();
+    }
+  }
+};
+// moreTransaction();
+
+mainFunc();
