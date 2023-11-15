24615b3f0307610339352646627c76c51b34f0ec
Ahmad Saboor <iamahmadsaboor@gmail.com>
2023-11-15 17:17:01 +0500

    Next Js Assignment Added

---
+++ Next JS Assignments/Assignment-1 Panaverse Clone/app/page.tsx
@@ -0,0 +1,310 @@
+import Image from "next/image";
+import PavaverseLogo from "@/public/panaverseLogo.png";
+import a from "next/link";
+export default function Home() {
+  return (
+    <div>
+      <div className="custom-bg">
+        <div>
+          <h1 className="text-[3.25rem] text-center md:px-[4em] mb-[0.2em] pt-12 font-semibold">
+            Certified Web 3.0 and Metaverse Developer: A Nationwide Program in
+            Karachi, Lahore, Islamabad, and Peshawar
+          </h1>
+          <p className="text-gray-300 text-xl">
+            Getting Ready for the Next Generation and Future of the Internet -
+            Join a 13 Trillion Dollar Industry with 5 Billion Users
+          </p>
+        </div>
+      </div>
+      <div className="my-0 mx-auto py-[2rem] px-[6rem] max-w-[64rem]">
+        <h1 className=" text-[#159957] text-[2.2em] mb-[1rem]">
+          Certified Web 3.0 and Metaverse Developer: A Nationwide Program in
+          Karachi, Lahore, Islamabad, and Peshawar
+        </h1>
+        <Image src={PavaverseLogo} alt="PanaverseLogo" className="" />
+        <h2 className="text-[#159957] text-[1.5em] mb-[1rem]">
+          The Future of the Web is Web 3.0, Metaverse, and Edge Computing.
+          Panaverse DAO is a movement to spread these technolgies globally. It
+          is community of Web 3 and Metaverse developers, designers, trainers,
+          startup founders and service providers.
+        </h2>
+        <a
+          href="https://www.piaic.org/"
+          className="text-[1.7em] text-[#1E6BB8] hover:underline "
+        >
+          Admissions Now Open in Karachi, Lahore, Islamabad, and Peshawar
+        </a>{" "}
+        <br />
+        <a
+          href="https://docs.google.com/document/d/13Z4tMKQmZpMEsOc1Y_qAXbOstRolGIFupHxQFgAFl98/edit"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+        >
+          This document on Google Docs
+        </a>
+        <p className="py-4 text-[#606C71] text-[1.1rem]">
+          The internet is without a doubt the most important technological
+          development in human history. Web3 and metaverse technologies expand
+          the internet as we know it by introducing novel features and
+          advancements. Metaverse will make use of all aspects of modern
+          technology, including 3D, VR, AR, AI, blockchain, cloud computing,
+          voice computing, ambient computing, and more. <br /> <br />
+          Citi is the latest Wall Street business to give a positive prognosis
+          for Web 3.0 and the Metaverse, terms used to depict a future internet
+          vision centred on decentralised technologies and virtual worlds. Citi
+          stated in a March 2022 research paper that the metaverse economy might
+          have a total addressable market of up to $13 trillion and five billion
+          people by 2030.
+        </p>
+        <h2 className="text-[#159957] text-2xl pb-4 pt-4">
+          The Program in a Nutshell: Earn While You Learn
+        </h2>
+        <p className="text-[#606C71] mb-[1em] text-[1.1rem]">
+          In this brand-new type of curriculum, students will learn how to make
+          money and boost exports in the classroom and will begin doing so
+          within six months of the program’s beginning. It resembles a cross
+          between a corporate venture and an educational project.
+        </p>
+        <h2 className="text-[#159957] text-2xl pb-4 pt-4">
+          Program of Studies
+        </h2>
+        <p className="text-[#606C71] mb-[1em] text-[1.1rem]">
+          This curriculum is intended for beginners who want to learn software
+          development from the ground up. The first two quarters are shared by
+          all specialities and are dedicated to studying Object-Oriented
+          Programming and cutting-edge Full-Stack Web 2.0 development. It is
+          going to be a year-long hybrid programme that includes both onsite and
+          online classes and is divided into four quarters of 13 weeks each. The
+          emphasis will be on hands-on learning by educating students to produce
+          projects. To accommodate everyone, courses will be held primarily on
+          weekends or after 6:00 p.m. (Pakistan Time) on weekdays. It employs a
+          hybrid teaching format, with core onsite classes complemented by
+          online Zoom laboratories and recorded videos.
+        </p>
+        <h2 className="text-[#159957] text-2xl pb-4 pt-4">
+          Core Courses (Common in All Specializations)
+        </h2>
+        <p className="text-[#606C71] mb-[1em] text-[1.1rem]">
+          Every participant of the program will start by completing the
+          following two core courses:
+        </p>
+        <a
+          href="https://github.com/panaverse/panaverse.github.io#quarter-i-core"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-6"
+        >
+          Quarter I (Core) CS-101: Object-Oriented Programming using TypeScript
+        </a>{" "}
+        <br /> <br />
+        <a
+          href="https://github.com/panaverse/panaverse.github.io#quarter-ii-core"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+        >
+          Quarter II (Core) W2-201: Developing Planet-Scale Web 2.0 Serverless
+          Cloud Cloud Apps and APIs using Next.js 13 and Cloud Development Kit
+          (CDK) for Terraform
+        </a>
+        <h2 className="text-[#159957] text-2xl pb-4 pt-4">
+          Specialized Tracks
+        </h2>
+        <p className="text-[#606C71] mb-[1em] text-[1.1rem]">
+          After completing the first two quarters the participants will select
+          one or more specializations consisting of two courses each:
+        </p>
+        <h2 className="text-[#159957] text-2xl pb-4 pt-4">
+          Web 3.0 (Blockchain) and Metaverse Specialization
+        </h2>
+        <p className="text-[#606C71] mb-[1em] text-[1.1rem]">
+          This Web 3.0 and Metaverse specialization focuses on developing
+          full-stack Web 3.0 and Metaverse experiences for the next generation
+          of the internet by specializing in building worlds that merge the best
+          of cutting-edge decentralized distributed blockchains with 3D
+          metaverse client experiences.
+        </p>
+        <a
+          href="https://www.panaverse.co/"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-6"
+        >
+          Quarter III W3-351: Developing Smart Contracts and Planet-Scale Web
+          3.0 Dapps
+        </a>{" "}
+        <br /> <br />
+        <a
+          href="https://www.panaverse.co/"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+        >
+          Quarter IV MV-361: Developing Planet-Scale Open Virtual and Augmented
+          Metaverse Experiences
+        </a>
+        <h2 className="text-[#159957] text-2xl pb-4 pt-4">
+          Artificial Intelligence (AI) and Deep Learning Specialization
+        </h2>
+        <p className="text-[#606C71] mb-[1em] text-[1.1rem]">
+          The AI and Deep Learning specialization focuses on building and
+          deploying intelligent APIs using OpenAI models and building custom
+          Deep Learning Tensorflow models.
+        </p>
+        <a
+          href="https://www.panaverse.co/"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-6"
+        >
+          Quarter III AI-351: Developing Planet-Scale Intelligent APIs and
+          Python Programming
+        </a>
+        <br /> <br />
+        <a
+          href="https://www.panaverse.co/"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+        >
+          Quarter IV AI-361: Deep Learning and MLOps
+        </a>
+        <h2 className="text-[#159957] text-2xl pb-4 pt-4">
+          Cloud-Native Computing Specialization
+        </h2>
+        <p className="text-[#606C71] mb-[1em] text-[1.1rem]">
+          The Cloud-Native Computing Specialization focuses on Containers,
+          Kubernetes, and CDK for Kubernetes.
+        </p>
+        <a
+          href="https://www.panaverse.co/"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-6"
+        >
+          Quarter III CN-351: Certified Kubernetes Application Developer (CKAD)
+        </a>
+        <br /> <br />
+        <a
+          href="https://www.panaverse.co/"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+        >
+          Quarter IV CN-361: Developing Multi-Cloud APIs using CDK for Terraform
+        </a>
+        <h2 className="text-[#159957] text-2xl pb-4 pt-4">
+          Ambient Computing and IoT Specialization{" "}
+        </h2>
+        <p className="text-[#606C71] mb-[1em] text-[1.1rem]">
+          The Ambient Computing and IoT Specialization focuses on building Smart
+          Homes, Offices, Factories, and Cities using Voice computing, Matter
+          and Embedded Devices.
+        </p>
+        <a
+          href="https://www.panaverse.co/"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-6"
+        >
+          Quarter III AC-351: Ambient Computing with Voice Assistants and Matter
+          Devices
+        </a>
+        <br /> <br />
+        <a
+          href="https://www.panaverse.co/"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+        >
+          Quarter IV AC-361: Embedded Programming using C and Rust
+        </a>
+        <h2 className="text-[#159957] text-2xl pb-4 pt-4">
+          The Outcome for Participants of the Program
+        </h2>
+        <p className="text-[#606C71] mb-[1em] text-[1.1rem]">
+          The graduates of this program will own products (Full-Stack App
+          Templates, AR and VR Experiences, and APIs) that are marketed globally
+          by the Panaverse DAO and, if they like, will also be able to start off
+          by offering services at a rate of $50 per hour ($96,000 per year).
+          This would give Pakistani professionals and students a fantastic
+          opportunity to better their financial situation while also giving the
+          economy a much-needed boost by expanding software exports.
+        </p>
+        <a
+          href="https://content.techgig.com/hiring/top-5-metaverse-jobs-that-will-rule-the-future-of-tech-industry/articleshow/87581325.cms"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-6"
+        >
+          Top 5 ‘Metaverse’ jobs that will rule the future of tech industry
+        </a>
+        <br /> <br />
+        <a
+          href="https://web3.career/web3-salaries/blockchain-developer"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+        >
+          Blockchain Developer Salary - Jun 2022
+        </a>
+        <br /> <br />
+        <a
+          href="https://thedefiant.io/web3-soaring-salaries/"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+        >
+          Web3 Salaries Soar to $750,000 for Rank-and-File Devs
+        </a>
+        <br /> <br />
+        <a
+          href="https://newzoo.com/insights/trend-reports/newzoo-report-on-metaverse-blockchain-gaming-nft-2022"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+        >
+          The Metaverse, Blockchain Gaming, and NFTs: Navigating the Internet’s
+          Uncharted Waters
+        </a>
+        <br /> <br />
+        <a
+          href="https://www.blockchain-council.org/metaverse/how-to-become-metaverse-developer/"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+          target="_blank"
+        >
+          How To Become Metaverse Developer: Scope, Skills, and Salary
+        </a>
+        <h2 className="text-[#159957] text-2xl pb-4 pt-4">
+          Relevant Information Links
+        </h2>
+        <a
+          href="https://www.piaic.org/"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-6"
+          target="_blank"
+        >
+          Admission Website
+        </a>
+        <br /> <br />
+        <a
+          href="https://www.piaic.org/"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+          target="_blank"
+        >
+          Facebook Group
+        </a>
+        <br /> <br />
+        <a
+          href="https://www.youtube.com/@panaverse/streams"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+          target="_blank"
+        >
+          YouTube Live Channel
+        </a>
+        <br /> <br />
+        <a
+          href="https://twitter.com/Panaverse_edu"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+          target="_blank"
+        >
+          Twitter
+        </a>
+        <br /> <br />
+        <a
+          href="https://github.com/panaverse"
+          className="text-[1.2em] text-[#1E6BB8] hover:underline my-4"
+          target="_blank"
+        >
+          GitHub Repos
+        </a>
+        <h2 className="text-[#159957] text-3xl pb-4 pt-4">
+          Detailed Course Syllabus
+        </h2>{" "}
+        <h2 className="text-[#159957] text-2xl pb-4 pt-4">Quarter I (Core)</h2>{" "}
+        <h2 className="text-[#159957] text-2xl pb-4 pt-4">
+          CS-101: Object-Oriented Programming using TypeScript
+        </h2>{" "}
+        <p className="text-[#606C71] mb-[1em] text-[1.1rem]">
+          Duration: 13 Weeks <br /> <br /> Course Description: <br /> <br /> We
+          will start the program by learning the fundamentals of Object-Oriented
+          programming using JavaScript and TypeScript. We will also understand
+          the latest Web trends i.e. Web 3.0 and Metaverse concepts and try to
+          understand their working from the perspective of the users. <br />{" "}
+          <br />
+          Course Outline: <br /> <br />
+        </p>
+      </div>
+    </div>
+  );
+}
